053195c7e3864fdb3b555c4f8f17ed01191cf054
Sarah Elzinga <sevans924@gmail.com>
2019-03-25 01:12:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ holiday-hash.map do |season, holiday|
       holiday
     end
   end
-  holiday.flatten
+  holiday.flatten.compact
     # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
