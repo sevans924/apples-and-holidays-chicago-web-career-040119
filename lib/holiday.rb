28d8d279a88ec2feec67a5cecb02e3e78edccf69
Sarah Elzinga <sevans924@gmail.com>
2019-03-25 00:28:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:summer][:memorial_day] = supply
+  holiday_hash[:summer][:memorial_day] << supply
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
